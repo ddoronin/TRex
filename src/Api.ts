875091b0df559e72e161d15777801d370cd2abf0
Dmitry_Doronin <doronindm@gmail.com>
2017-11-14 00:44:02 -0500

    simplify dependency injection

--- src/Api.ts
+++ src/Api.ts
@@ -1,2 +1,15 @@
 import ServiceLocator from './di/ServiceLocator';
-export default new ServiceLocator();
+
+import {AppController, IAppController} from "./controllers/AppController";
+import {ContactsController, IContactsController} from "./controllers/ContactsController";
+import {ISearchController, SearchController} from "./controllers/SearchController";
+
+const api = new ServiceLocator();
+
+(function(api: ServiceLocator) {
+    api.bind<IAppController>('IAppController').to(new AppController());
+    api.bind<IContactsController>('IContactsController').to(new ContactsController());
+    api.bind<ISearchController>('ISearchController').to(new SearchController());
+})(api);
+
+export default api;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import '../styles/main.scss';
-
-// Inject dependencies
-import Api from './Api';
-import {inject} from './inject';
-inject(Api);
-
 import App from './App';
 
 ReactDOM.render(
